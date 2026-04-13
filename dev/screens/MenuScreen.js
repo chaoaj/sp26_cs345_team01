ffdfb298c65551be76162c2f67eaed87949ad849
andreacorrea2400 <corre2am@dukes.jmu.edu>
2026-04-13 08:23:14 -0400

    animated the background

--- dev/screens/MenuScreen.js
+++ dev/screens/MenuScreen.js
@@ -4,6 +4,12 @@ class MenuScreen extends Screen {
     super();
     this.mainMenuBg = Assets.backgroundImg;
     this.wildSlideLogo = Assets.logoImg;
+    this.tree1Img = Assets.tree1Img;
+    this.tree2Img = Assets.tree2Img;
+    this.leavesImg = Assets.leavesImg;
+    this.treeOffset = 0;
+    this.leafOffset = 0;
+    this.treeScale = 0.57;
   }
 
   onEnter() {
@@ -16,6 +22,42 @@ class MenuScreen extends Screen {
 
   draw() {
     imageMode(CENTER);
+    image(
+      this.mainMenuBg,
+      width / 2,
+      height / 2,
+      this.mainMenuBg.width * 0.8,
+      this.mainMenuBg.height * 0.8
+    );
+    this.treeOffset += 0.01;
+    this.leafOffset += 0.04;
+    let treeWind = Math.sin(this.treeOffset) * 2;
+    let leafWind = Math.sin(this.leafOffset) * 3;
+    let tree1W = this.tree1Img.width * this.treeScale;
+    let tree1H = this.tree1Img.height * this.treeScale;
+    let tree2W = this.tree2Img.width * this.treeScale;
+    let tree2H = this.tree2Img.height * this.treeScale;
+    image(
+      this.tree1Img,
+      -70 + treeWind,
+      height - tree1H,
+      tree1W,
+      tree1H
+    );
+    image(
+      this.tree2Img,
+      width - tree2W + 70 + treeWind,
+      height - tree2H,
+      tree2W,
+      tree2H
+    );
+    image(
+      this.leavesImg,
+      leafWind,
+      0,
+      width,
+      height
+    );
     image(this.mainMenuBg, width / 2, height / 2, this.mainMenuBg.width * 0.8, this.mainMenuBg.height * 0.8)
 
     imageMode(CENTER);
